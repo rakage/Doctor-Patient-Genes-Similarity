769f9e1612314f3f8f256d52695107775e3c3e2c
Raka Luthfi <raka.luthfi@bankmega.com>
2024-07-21 01:49:46 +0700

    faw

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
